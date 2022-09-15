7de4bff131978fa6baac664f021dc2ce22a0dd52
Sten Hougaard <netsi1964@gmail.com>
2022-09-15 22:46:44 +0200

    Changed to my data

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 import blog, { ga, redirects } from "https://deno.land/x/blog@0.5.0/blog.tsx";
 
 blog({
-  title: "Ryan Dahl",
-  author: "Ryan Dahl",
-  avatar: "./ry.jpg",
+  title: "Sten Hougaard",
+  author: "Sten Hougaard",
+  avatar: "./netsi1964.png",
   avatarClass: "full",
   links: [
-    { title: "Email", url: "mailto:ry@tinyclouds.org" },
-    { title: "GitHub", url: "https://github.com/ry" },
+    { title: "Email", url: "mailto:netsi1964@gmail.com" },
+    { title: "GitHub", url: "https://github.com/netsi1964" },
   ],
   background: "#fff",
   middlewares: [
-    ga("UA-91675022-1"),
+    ga(""),
     redirects({
       "iocp-links.html": "iocp_links",
       "rant.html": "rant",
